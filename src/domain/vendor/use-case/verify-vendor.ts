69156751e3f3e5a6b4ffeeb9999846d4005b9e47
zufichris <zufichris@gmail.com>
2025-03-08 09:39:17 +0400

    updated verify-vendor.ts

--- src/domain/vendor/use-case/verify-vendor.ts
+++ src/domain/vendor/use-case/verify-vendor.ts
@@ -1,6 +1,6 @@
 import { TVendor } from "../../../data/entity/vendor";
-import { EStatusCodes } from "../../../global/enum";
-import { AuthContext, BaseUseCase, handleUseCaseError, UseCaseResult } from "../../../global/use-case";
+import { EStatusCodes } from "../../../shared/enum";
+import { AuthContext, BaseUseCase, handleUseCaseError, UseCaseResult } from "../../../shared/use-case";
 import { IVendorRepository } from "../repository";
 
 export class VerifyVendorUseCase implements BaseUseCase<{ id: string, status: "APPROVED" | "REJECTED", reason?: string }, TVendor, AuthContext> {
