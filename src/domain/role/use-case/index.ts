b27e7e0f7ed32c097c5d7dbee7bcc8fdb36354eb
zufichris <zufichris@gmail.com>
2025-03-08 09:39:17 +0400

    updated index.ts

--- src/domain/role/use-case/index.ts
+++ src/domain/role/use-case/index.ts
@@ -4,6 +4,8 @@ import { GetRoleUseCase } from "./get-role";
 import { QueryRolesUseCase } from "./query-roles";
 import { UpdateRolePermissionsUseCase } from "./update-role-permissions";
 import { DeleteRoleUseCase } from "./delete-role"
+import { InitializeRoleUseCase } from "./initialize";
+import { GetPermissionsUseCase } from "./get-permissions";
 
 export default class RoleUseCase {
     public readonly create: CreateRoleUseCase;
@@ -11,6 +13,8 @@ export default class RoleUseCase {
     public readonly query: QueryRolesUseCase;
     public readonly update: UpdateRolePermissionsUseCase;
     public readonly delete: DeleteRoleUseCase;
+    public readonly getPermissions: GetPermissionsUseCase;
+    private readonly initialize: InitializeRoleUseCase;
 
     constructor(private readonly roleRepository: IRoleRepository) {
         this.create = new CreateRoleUseCase(this.roleRepository);
@@ -18,5 +22,16 @@ export default class RoleUseCase {
         this.query = new QueryRolesUseCase(this.roleRepository);
         this.update = new UpdateRolePermissionsUseCase(this.roleRepository);
         this.delete = new DeleteRoleUseCase(this.roleRepository);
+        this.initialize = new InitializeRoleUseCase(this.roleRepository)
+        this.getPermissions = new GetPermissionsUseCase(this.roleRepository)
+    }
+    async initializeRoles() {
+        const [admin, user, vendor] = await Promise.all([
+            await this.initialize.execute("Admin"),
+            await this.initialize.execute("User"),
+            await this.initialize.execute("Vendor")
+        ])
+
+        return admin.success && user.success && vendor.success
     }
 }
