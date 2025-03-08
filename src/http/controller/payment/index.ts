b6e9ef1d51a8f96af1f63f785cdef1ae8c1b88e0
zufichris <zufichris@gmail.com>
2025-03-08 09:39:17 +0400

    updated index.ts

--- src/http/controller/payment/index.ts
+++ src/http/controller/payment/index.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { validateData } from "../../../util/functions";
-import { IResponseData, IResponseDataPaginated, IQueryFilters } from "../../../global/entity";
-import { EStatusCodes } from "../../../global/enum";
+import { IResponseData, IResponseDataPaginated, IQueryFilters } from "../../../shared/entity";
+import { EStatusCodes } from "../../../shared/enum";
 import PaymentUseCase from "../../../domain/payment/use-case";
 import { PaymentRepositoryImpl } from "../../../data/orm/repository-implementation/payment";
 import { CreatePaymentDTO, CreatePaymentSchema, UpdatePaymentDTO, UpdatePaymentSchema } from "../../../data/dto/payment";
