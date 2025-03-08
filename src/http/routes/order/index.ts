9a118e98c4c32504541a2f568bc21801e17ea067
zufichris <zufichris@gmail.com>
2025-03-08 09:39:18 +0400

    updated index.ts

--- src/http/routes/order/index.ts
+++ src/http/routes/order/index.ts
@@ -3,11 +3,11 @@ import { orderControllers } from '../../controller/order';
 import { authMiddleware } from '../../middleware/auth';
 
 const router = Router();
-router.use(authMiddleware.requireAuth, authMiddleware.requirePermission("order", "manage_own"),)
+router.use(authMiddleware.requireAuth, authMiddleware.requirePermission("order", "manage"),)
 router.route("/")
     .post(orderControllers.createOrder)
     .get(orderControllers.queryOrders);
-router.use(authMiddleware.requirePermission("order", "manage_own")).route("/:orderId")
+router.use(authMiddleware.requirePermission("order", "manage")).route("/:orderId")
     .get(orderControllers.getOrder)
     .put(orderControllers.updateOrder)
     .post(orderControllers.cancelOrder);
