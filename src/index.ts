a7b9deaaa24ac10c20aa3381703993d35fd5ba4b
zufichris <zufichris@gmail.com>
2025-03-08 09:39:18 +0400

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,15 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors"
-import router from "./http/routes/route_v1";
-import { Start } from "./http/server/httpServer";
-import { errorHandler, notFound } from "./http/middleware/error";
-import { DB } from "./config/db-config";
+import { StartServer } from "./http/server/httpServer";
+import { initializeApp } from "./start";
 import { env } from "./config/env";
-import { loggerMiddleware } from "./http/middleware/logger";
 
-new DB(env.mongo_uri).connect();
+async function main() {
+    try {
+        const app = await initializeApp();
+        StartServer(app, Number(env.port));
+    } catch (error) {
+        console.error("Failed to start the application:", error);
+        process.exit(1);
+    }
+}
 
-const app = express();
-app.use(cors())
-app.use(express.json({}))
-app.use(
-    express.urlencoded({
-        extended: true,
-    })
-);
-app.use(cookieParser());
-app.use(loggerMiddleware)
-
-app.use("/api/v1", router);
-app.get("/", (_, res) => {
-    res.send(`
-        <h1>Welcome to Vendor Verse API</h1></br>
-        <a href='/api/v1'>Explore API</a>
-    `)
-})
-app.use(notFound);
-app.use(errorHandler);
-Start(app);
-export default app;
+main();
