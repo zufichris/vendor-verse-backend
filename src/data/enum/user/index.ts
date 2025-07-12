98dc2b13e1a47e4dec1e97b66e170da315a22f5d
zufichris <zufichris@gmail.com>
2025-07-12 04:14:05 +0400

    changed architecture

--- src/data/enum/user/index.ts
+++
@@ -1,5 +0,0 @@
-export enum Role {
-    User = "User",
-    Vendor = "Vendor",
-    Admin = "Admin"
-}
--- src/data/enum/vendor/index.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod"
-export const EBusinessTypes = z.enum(
-    ["LLC", "SOLE PROPRIETOR", "COPORATION"]
-)
-export const EVerificationTypes = z.enum(
-    ["NATIONAL ID", "PASSPORT", "DRIVING LISCENSE"]
-)
