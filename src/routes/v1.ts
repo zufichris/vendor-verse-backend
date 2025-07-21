d418b015451b4a57ca535efe2f4cd8f647f241d0
zufichris <zufichris@gmail.com>
2025-07-21 08:41:39 +0400

    routes: update v1 API routes

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { intitUserModule } from "../modules/user";
 import { initProductModule } from "../modules/product";
+import { initOrderModule } from "../modules/order/";
 
 const router = express.Router();
 
 router.use("/users", intitUserModule());
 router.use("/products", initProductModule());
+router.use("/orders", initOrderModule());
+
 export const routesv1 = router;
