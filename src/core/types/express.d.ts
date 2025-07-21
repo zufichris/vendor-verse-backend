7f21fe8d1be2c4531a01c3eb7c06547208077737
zufichris <zufichris@gmail.com>
2025-07-21 08:41:39 +0400

    types: extend Express types definitions

--- src/core/types/express.d.ts
+++ src/core/types/express.d.ts
@@ -1,10 +1,12 @@
 import * as express from "express";
 import { User } from "../../modules/user";
+import { Locale } from "../../core/middleware/";
 
 declare global {
-    namespace Express {
-        interface Request {
-            user?: User;
-        }
+  namespace Express {
+    interface Request {
+      user?: User;
+      locale: Locale;
     }
+  }
 }
