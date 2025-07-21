9d97f5008882e36859a6712074d529bad1a8a184
zufichris <zufichris@gmail.com>
2025-07-21 08:41:44 +0400

    middleware: add middleware index file

---
+++ src/core/middleware/index.ts
@@ -0,0 +1,5 @@
+export * from "./auth.middleware";
+export * from "./error.middleware";
+export * from "./localization.middleware";
+export * from "./ratelimit.middleware";
+export * from "./validation.middleware";
