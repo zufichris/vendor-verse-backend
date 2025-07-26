a067921037708694ec8d17aa9a33e15272159216
zufichris <zufichris@gmail.com>
2025-07-26 12:53:09 +0400

    fix(payment): improve service logic or external payment integration

--- src/modules/order/payment.service.ts
+++ src/modules/order/payment.service.ts
@@ -4,31 +4,30 @@ import { env } from "../../config";
 import { AppError } from "../../core/middleware/error.middleware";
 
 export class PaymentService {
-    constructor(private readonly stripe: Stripe.Stripe) { }
+  constructor(private readonly stripe: Stripe.Stripe) {}
+  async createStripeCheckoutSession(order: Order) {
+    try {
+      const line_items: Stripe.Stripe.Checkout.SessionCreateParams.LineItem[] =
+        order.items.map((item) => ({
+          price_data: {
+            currency: "usd",
+            product_data: { name: item.name },
+            unit_amount: Math.round(item.price * 100),
+          },
+          quantity: item.quantity,
+        }));
 
-    async createStripeCheckoutSession(order: Order) {
-        try {
-            const line_items: Stripe.Stripe.Checkout.SessionCreateParams.LineItem[] =
-                order.items.map((item) => ({
-                    price_data: {
-                        currency: "usd",
-                        product_data: { name: item.name },
-                        unit_amount: Math.round(item.price * 100),
-                        product: item.productId,
-                    },
-                    quantity: item.quantity,
-                }));
-
-            const session = await this.stripe.checkout.sessions.create({
-                line_items,
-                mode: "payment",
-                metadata: { orderId: order.id },
-                success_url: `${env.client_url}/success`,
-                cancel_url: `${env.client_url}/cancel`,
-            });
-            return session;
-        } catch (error) {
-            throw AppError.badGateway("stripe payment failed", error);
-        }
+      const session = await this.stripe.checkout.sessions.create({
+        line_items,
+        mode: "payment",
+        metadata: { orderId: order.id },
+        success_url: `${env.client_url}/home`,
+        cancel_url: `${env.client_url}/checkout`,
+      });
+      return {url:session.url};
+    } catch (error) {
+      console.log("Stripe Error", error);
+      throw AppError.badGateway("stripe payment failed", error);
     }
+  }
 }
